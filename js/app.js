41ac1fb8cc22b5f62c6fe076a070db94e8017f15
Rick <trongtri.1975s@gmail.com>
2025-06-17 00:14:47 +0700

    Update vercel deploy

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 var app = angular.module("myApp", ["ngRoute"]);
-let API = "/";
+let API = "/api/data";
 
 app.factory("UserService", function () {
     return {
@@ -14,228 +14,137 @@ app.factory("UserService", function () {
         },
     };
 });
-app.run([
-    "$rootScope",
-    "UserService",
-    function ($rootScope, UserService) {
-        $rootScope.user = UserService.getUser(); 
-    },
 
+app.run([
+    "$rootScope", "UserService",
+    function ($rootScope, UserService) {
+        $rootScope.user = UserService.getUser();
+    }
 ]);
 
-// get-data-from-JSON
 app.controller("appCtrl", function ($scope, $location, $http, UserService) {
-    function getDataFromJSON() {
-        $http.get(API + "cakes").then(
-            function (response) {
-                $scope.cakes = response.data;
-            },
-            function (response) {
-                console.log(response);
-            }
-        );
-        $http.get(API + "categories").then(
-            function (response) {
-                $scope.categories = response.data;
-            },
-            function (response) {
-                console.log(response);
-            }
-        );
+    function getData() {
+        $http.get(API).then(function (response) {
+            $scope.cakes = response.data.cakes;
+            $scope.categories = response.data.categories;
+        }, function (response) {
+            console.log(response);
+        });
     }
-    getDataFromJSON();
+    getData();
+
     $scope.getCake = function (cake) {
         $location.path("/product-detail/" + cake.id);
     };
+
     $scope.logOut = function () {
         UserService.removeUser();
         window.location.reload();
-    }
-    
+    };
 });
 
 app.controller("menuCtrl", function ($scope, $location, $http) {
-    function getDataFromJSON() {
-        $http.get(API + "cakes").then(
-            function (response) {
-                $scope.cakes = response.data;
-            },
-            function (response) {
-                console.log(response);
-            }
-        );
-        $http.get(API + "categories").then(
-            function (response) {
-                $scope.categories = response.data;
-            },
-            function (response) {
-                console.log(response);
-            }
-        );
+    function getData() {
+        $http.get(API).then(function (response) {
+            $scope.cakes = response.data.cakes;
+            $scope.categories = response.data.categories;
+        }, function (response) {
+            console.log(response);
+        });
     }
-    getDataFromJSON();
+    getData();
+
     $scope.setOrder = function (categoryName) {
-        if ($scope.order == categoryName) {
-            $scope.order = "";
-        } else {
-            $scope.order = categoryName;
-        }
+        $scope.order = ($scope.order === categoryName) ? "" : categoryName;
     };
+
     $scope.getCake = function (cake) {
         $location.path("/product-detail/" + cake.id);
     };
 });
 
+app.controller("productCtrl", function ($scope, $routeParams, $http, $location) {
+    $http.get(API).then(function (response) {
+        $scope.cakes = response.data.cakes;
+        $scope.cake = $scope.cakes.find(c => c.id === $routeParams.productID);
+    }, function (response) {
+        console.log(response);
+    });
 
-app.controller(
-    "productCtrl",
-    function ($scope, $routeParams, $http, $location) {
-        let productID = $routeParams.productID;
-        $http.get(API + "cakes/" + productID).then(function (response) {
-            $scope.cake = response.data;
-        });
-        $http.get(API + "cakes").then(
-            function (response) {
-                $scope.cakes = response.data;
-            },
-            function (response) {
-                console.log(response);
-            }
+    $scope.getCake = function (cake) {
+        $location.path("/product-detail/" + cake.id);
+    };
+});
+
+app.controller("userCtrl", function ($scope, $rootScope, $http, $timeout, $location, UserService) {
+    $scope.users = [];
+
+    $http.get(API).then(function (response) {
+        $scope.users = response.data.users;
+    }, function (response) {
+        console.log(response);
+    });
+
+    $scope.login = function () {
+        if (checkEmpty($scope.user)) {
+            $scope.checkAuth();
+        } else {
+            alert("Vui lòng nhập email và password");
+        }
+    };
+
+    $scope.checkAuth = function () {
+        let found = $scope.users.find(user =>
+            user.email === $scope.user.email && user.password === $scope.user.password
         );
-        $scope.getCake = function (cake) {
-            $location.path("/product-detail/" + cake.id);
-        };
-    }
-);
-
-
-app.controller(
-    "userCtrl",
-    function ($scope, $rootScope, $http, $timeout, $location, UserService) {
-        $scope.getUsers = function () {
-            $http.get(API + "users/").then(
-                function (response) {
-                    $scope.users = response.data;
-                },
-                function (response) {
-                    console.log(response);
-                }
-            );
-        };
-        $scope.addUser = function (newUser) {
-            $http.post(API + "users/", newUser).then(
-                function (response) {
-                    $scope.getUsers();
-                },
-                function (response) {
-                    console.log(response);
-                }
-            )
+        if (found) {
+            alert("Mừng bạn trở lại " + found.name);
+            UserService.saveUser(found);
+            $rootScope.user = found;
+            $timeout(() => $location.path("/"), 1000);
+        } else {
+            alert("Sai tên đăng nhập hoặc mật khẩu");
         }
-        $scope.getUsers();
-        $scope.login = function () {
-            if (checkEmpty($scope.user)) {
-                $scope.checkAuth();
-            } else {
-                alert("Vui lòng nhập email và password");
-            }
-        };
-        $scope.checkAuth = function () {
-            if ($scope.isUserInUsers()) {
-                alert("Mừng bạn trở lại " + $scope.user.name);
-                $rootScope.user = UserService.getUser(); 
-                $timeout(function () {
-                    $location.path("/");
-                }, 2000);
-            } else {
-                alert("Sai tên đăng nhập hoặc mật khẩu");
-            }
-        };
-        $scope.isUserInUsers = function () {
-            let check = false;
-            $scope.users.forEach(function (user) {
-                if (
-                    $scope.user.email === user.email &&
-                    $scope.user.password === user.password
-                ) {
-                    UserService.saveUser(user);
-                    $scope.user = user;
-                    check = true;
-                }
-            });
-            return check;
-        };
+    };
 
-        $scope.signUp = function () {
-            if (checkEmpty($scope.newUser)) {
-                alert("Đăng ký thành công, bạn sẽ được chuyển đến trang đăng nhập");
-                $scope.addUser($scope.newUser);
-                $timeout(function () {
-                    $location.path("/login");
-                }, 2000);
-            } else {
-                alert("Vui lòng điền đầy đủ thông tin");
-            }
+    $scope.signUp = function () {
+        if (checkEmpty($scope.newUser)) {
+            alert("Đăng ký thành công, nhưng đây là data cứng nên không lưu!");
+            $timeout(() => $location.path("/login"), 1000);
+        } else {
+            alert("Vui lòng điền đầy đủ thông tin");
         }
-    }
-);
-//scoll-on-top
+    };
+});
+
+// scroll on top
 app.run([
-    "$rootScope",
-    "$anchorScroll",
+    "$rootScope", "$anchorScroll",
     function ($rootScope, $anchorScroll) {
         $rootScope.$on("$routeChangeSuccess", function () {
             $anchorScroll();
         });
-    },
+    }
 ]);
 
-// config-SPA
 app.config(function ($routeProvider) {
     $routeProvider
-        .when("/", {
-            templateUrl: "home.html",
-            controller: "appCtrl",
-        })
-        .when("/about-us", {
-            templateUrl: "about-us.html",
-        })
-        .when("/RickMenu", {
-            templateUrl: "menu.html",
-            controller: "menuCtrl",
-        })
-        .when("/product-detail/:productID", {
-            templateUrl: "products-detail.html",
-            controller: "productCtrl",
-        })
-        .when("/contact", {
-            templateUrl: "contact.html",
-        })
-        .when("/login", {
-            templateUrl: "login.html",
-            controller: "userCtrl",
-        })
-        .when("/sign-up", {
-            templateUrl: "sign-up.html",
-            controller: "userCtrl",
-        })
-        .when("/cart", {
-            templateUrl: "cart.html",
-        })
-        .when("/check-out", {
-            templateUrl: "check-out.html",
-        })
-        .when("/404", {
-            templateUrl: "404.html",
-        })
-        .otherwise({
-            redirectTo: "/404",
-        });
+        .when("/", { templateUrl: "home.html", controller: "appCtrl" })
+        .when("/about-us", { templateUrl: "about-us.html" })
+        .when("/RickMenu", { templateUrl: "menu.html", controller: "menuCtrl" })
+        .when("/product-detail/:productID", { templateUrl: "products-detail.html", controller: "productCtrl" })
+        .when("/contact", { templateUrl: "contact.html" })
+        .when("/login", { templateUrl: "login.html", controller: "userCtrl" })
+        .when("/sign-up", { templateUrl: "sign-up.html", controller: "userCtrl" })
+        .when("/cart", { templateUrl: "cart.html" })
+        .when("/check-out", { templateUrl: "check-out.html" })
+        .when("/404", { templateUrl: "404.html" })
+        .otherwise({ redirectTo: "/404" });
 });
 
 let checkEmpty = function (obj) {
     for (let key in obj) {
-        if (obj[key] === undefined || obj[key] === null || obj[key] === "") {
+        if (!obj[key]) {
             return false;
         }
     }
